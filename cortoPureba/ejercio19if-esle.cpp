2e5a46e6c076386a558683029b22c374ab824eac
Melara00180825 <00034725@uca.edu.sv>
2025-06-03 09:43:15 -0600

    prueba

---
+++ cortoPureba/ejercio19if-esle.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+using namespace std;
+
+int main (){
+    double tarifa=0.1;
+
+    cout<<"Welcome to the pay your energy house"<<endl;
+    cout<<"Please pay your bill "
+
+    return 0;
+}
